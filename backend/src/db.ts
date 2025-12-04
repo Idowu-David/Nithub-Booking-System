1de1ea6b9c70c1b89a16485cecc458ca2905e5be
Idowu-David <idowudavidodun@gmail.com>
2025-12-04 15:35:03 +0100

    live connection

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -14,14 +14,24 @@ const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   dialect: "postgres",
 });
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT || 8000),
-});
+const connectionConfig = isProduction
+  ? {
+      connectionString: connectionString,
+      // Render requires SSL to be explicitly set to reject unauthorized for secure connection
+      ssl: { rejectUnauthorized: false },
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT || 8000),
+    };
+
+const pool = new Pool(connectionConfig);
 
 pool.on("connect", () => {
   console.log("Database Connected Successfully");
